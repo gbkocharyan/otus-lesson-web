db9e60e2771448e413f03f241a0dcdef5b1032cb
gbkocharyan <g.b.kocharyan@gmail.com>
2025-04-25 18:40:14 +0400

    Add `@Path` annotation and URL handling for page navigation
    Introduced `@Path` and `@UrlTemplate` annotations to define URL structure for pages. Enhanced `AbsBasePage` to support dynamic URL generation and page navigation using these annotations. Configured project code style settings to ensure consistent multiline string formatting.

--- src/main/java/pages/AbsBasePage.java
+++ src/main/java/pages/AbsBasePage.java
@@ -1,11 +1,37 @@
 package pages;
 
+import annotations.Path;
 import common.AbsCommon;
 import org.openqa.selenium.WebDriver;
+
+import java.lang.annotation.Annotation;
 
 public abstract class AbsBasePage extends AbsCommon {
 
   public AbsBasePage(WebDriver driver) {
     super(driver);
   }
+
+  private String baseUrl = System.getProperty("baseUrl");
+
+  private <T extends Annotation> T getAnnotation(Class<T> annotationClass) {
+    if (getClass().isAnnotationPresent(annotationClass)) {
+      return getClass().getDeclaredAnnotation(annotationClass);
+    }
+    throw new RuntimeException(String.format("Class %s has no annotation @Path", getClass().getCanonicalName()));
+  }
+
+  private String getPath() {
+    return getAnnotation(Path.class).value();
+  }
+
+  public String getUrl() {
+    baseUrl = baseUrl.endsWith("/") ? baseUrl.substring(0, baseUrl.lastIndexOf("/")) : baseUrl;
+    return baseUrl + getPath();
+  }
+
+  public void open() {
+    driver.get(getUrl());
+  }
+
 }
--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -1,7 +1,9 @@
 package pages;
 
+import annotations.Path;
 import org.openqa.selenium.WebDriver;
 
+@Path("/")
 public class MainPage extends AbsBasePage {
 
   public MainPage(WebDriver driver) {
