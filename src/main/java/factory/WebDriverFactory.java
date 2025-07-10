6fc84895604d6e8b4c32dad778b7b08e0e9b3fb5
gevorg.kocharyan <gevorg.kocharyan@acba.am>
2025-07-10 17:59:30 +0400

    Add support for remote WebDriver in WebDriverFactory
    Updated the WebDriverFactory to optionally use a remote WebDriver instance for Chrome when a remote IP is specified. This allows compatibility with Selenoid by configuring the remote WebDriver URL. Also included error handling for invalid remote URLs.

--- src/main/java/factory/WebDriverFactory.java
+++ src/main/java/factory/WebDriverFactory.java
@@ -5,16 +5,34 @@ import factory.settings.ChromeSettings;
 import listeners.MouseListener;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.events.EventFiringDecorator;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class WebDriverFactory {
   private final String browserName = System.getProperty("browser");
+  private final String remoteIp = System.getProperty("remoteIp");
 
   public WebDriver create() {
-    switch(browserName) {
+    switch (browserName) {
       case "chrome":
-        return new EventFiringDecorator<>(new MouseListener()).decorate(
-            new ChromeDriver(new ChromeSettings().settings()));
+        ChromeOptions options = new ChromeSettings().settings();
+        try {
+          if (remoteIp != null && !remoteIp.isEmpty()) {
+            // Remote WebDriver (for Selenoid)
+            String remoteUrl = "http://" + remoteIp + "/wd/hub";
+            return new EventFiringDecorator<>(new MouseListener()).decorate(
+                new RemoteWebDriver(new URL(remoteUrl), options));
+          } else {
+            // Local WebDriver
+            return new EventFiringDecorator<>(new MouseListener()).decorate(
+                new ChromeDriver(options));
+          }
+        } catch (MalformedURLException e) {
+          throw new RuntimeException("Invalid Selenoid URL", e);
+        }
     }
     throw new BrowserNotSupportedException(browserName);
   }
